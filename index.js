55aa9103965fb12616837b65dc0a0f456abff148
Huimin <huiminzhang0111@hotmail.com>
2021-09-30 16:00:14 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('web 46 from index.js!!')
+require('dotenv').config() // this allows to inject fake environment variables
+
+const { PORT } = require('./config')
+
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet())
+
+server.get('/', (req, res) => {
+    res.send(`
+        <h1>web 46</h1>
+    `)
+})
+
+server.use('*', (req, res, next) => {
+    res.json({
+        message: "web 46 - Huimin Zhang"
+    })
+})
+
+server.listen(PORT || 5000, () => {
+    console.log(`listening on port ${PORT || 5000}`)
+})
